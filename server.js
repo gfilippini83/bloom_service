4340678b7ff0726b74927f70a866c28ce278d3e2
gfilippini83 <gfilippini4@gmail.com>
2020-12-21 18:39:56 -0500

    HTTPS

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const streamerRoutes = require('./server/routes/streamerRouter');
 const baseInfoRoutes = require('./server/routes/baseInfoRouter');
 
+var fs = require('fs')
+var https = require('https')
+
 mongoose.connect(`mongodb://${process.env.MONGO_USR}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/bloom?authSource=admin`);
 
 
@@ -18,10 +21,19 @@ app.use('/api/base-info', baseInfoRoutes);
 // app.use(app.router);
 // streamerRoutes.initialize(app)
 
-app.listen(4201, (err) => {
-  if (err) {
-    return console.log(err);
-  }
 
-  return console.log('My Node App listening on port 4201');
-});
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
+
+// app.listen(4201, (err) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   return console.log('My Node App listening on port 4201');
+// });
